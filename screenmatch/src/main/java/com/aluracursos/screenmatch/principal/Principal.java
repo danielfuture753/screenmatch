50ed60a9abd6847366f5d8cb34fb87574bc59cee
Daniel Salazar Garcia <daniel_hal1_@hotmail.com>
2024-05-13 21:36:47 -0600

    VERSION 3.1
    AQUI ESE MANEJO MUCHO LA CARPETA DE PRINCIPAL DONDE SE HACEN VARIOS CAMBIOS Y Comentarios
    YA QUE CADA SECCION HACE CIERTAS COSA, CHECAR YA QUE NINGUNA LINEA FUE ELIMINADA
    SOLO COMENTADA

--- screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -1,11 +1,19 @@
 package com.aluracursos.screenmatch.principal;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import javax.sound.sampled.SourceDataLine;
+
+import org.springframework.cglib.core.Local;
 
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
@@ -57,12 +65,17 @@ public class Principal {
                                                         .collect(Collectors.toList());
 
         //Topo 5 episodios
-        System.out.println("Top 5 episodios");
+/*         System.out.println("Top 5 episodios");
         datosEpisodios.stream()
                         .filter(e -> !e.Evaluacion().equalsIgnoreCase("N/A"))
+                        .peek(e-> System.out.println("Primer filtro (N/A)" + e))
                         .sorted(Comparator.comparing(DatosEpisodio::Evaluacion).reversed())
+                        .peek(e-> System.out.println("segunda ordenacion (M>m) mayor a menor" + e))
+                        .map(e -> e.titulo().toUpperCase())
+                        .peek(e-> System.out.println("Tercer filtro Mayusculas  (m>M)" + e))
                         .limit(5)
                         .forEach(System.out::println);
+ */
 
         //Convirtiendo los datos a una lista del tipo Episodio
         List<Episodio> episodios = temporadas.stream() //stream datos temporada
@@ -70,7 +83,50 @@ public class Principal {
                                         .map(d -> new Episodio(t.numero(),d)))
                             .collect(Collectors.toList());
 
-        episodios.forEach(System.out::println);
+//         episodios.forEach(System.out::println);
+
+
+/*          // Busqueda a partir de un año en especifico 
+         System.out.println("Por favor indique el año a partir del cual deseas ver los episodios");
+         var fecha = teclado.nextInt();
+         teclado.nextLine();
+
+         LocalDate fechaBusqueda = LocalDate.of(fecha, 1, 1);
+
+         DateTimeFormatter dtf  = DateTimeFormatter.ofPattern("dd/MM/yyyy"); 
+*/
+/*          episodios.stream()
+                .filter(e -> e.getFechaDeLanzamiento() != null && e.getFechaDeLanzamiento().isAfter(fechaBusqueda))
+                .forEach(e -> System.out.println("Temporada = " + e.getTemporada()
+                                                    + "Episodio = " + e.getTitulo()
+                                                    + "Fecha de lanzamiento = " + e.getFechaDeLanzamiento().format(dtf)
+                                                )
+                        ); 
+*/
+
+
+        //Busca espisodios por pedazo del titutlo
+/*         System.out.println("Por favior esccribe el titulo del espisodio que desea ver");
+        var pedazoTitulo = teclado.nextLine();
+        Optional<Episodio> episodioBuscar = episodios.stream() // [info_curso]  (file:///F:/CURSO/CURSOS/CLONE_GITHUB/screenmatch/screenmatch/info_curso.txt#L827)
+                 .filter(e -> e.getTitulo().toUpperCase().contains(pedazoTitulo.toUpperCase()))
+                 .findFirst(); //  [info_curso]  (file:///F:/CURSO/CURSOS/CLONE_GITHUB/screenmatch/screenmatch/info_curso.txt#L868)  [comentarios]  (file:///F:/CURSO/CURSOS/CLONE_GITHUB/screenmatch/screenmatch/Comentarios.txt#L9)
+        if(episodioBuscar.isPresent()){
+            System.out.println("Episodio encontrado");
+            System.out.println("Los datos son  " + episodioBuscar.get()); // [comentarios]  (file:///F:/CURSO/CURSOS/CLONE_GITHUB/screenmatch/screenmatch/Comentarios.txt#L11)
+        }else{
+            System.out.println("Episodio no encontrado");
+        }
+ */
+
+        Map<Integer , Double> evaluacionesPorTemporada = episodios.stream()
+                    .filter( e -> e.getEvaluacion() > 0.0)
+                    .collect(Collectors.groupingBy(Episodio::getTemporada, Collectors.averagingDouble(Episodio::getEvaluacion)));
+        System.out.println(evaluacionesPorTemporada);
+
     }
+
+   
+  
 
 }
